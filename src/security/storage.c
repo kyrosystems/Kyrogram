5dfb3857f914665b9ba7d9919a45fcded24a20fe
pon4ikisdonut <pontyarliliya@gmail.com>
2026-04-03 14:46:35 +0300

    i`m tired

--- src/security/storage.c
+++ src/security/storage.c
@@ -1,4 +1,7 @@
 #include "storage.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <mbedtls/aes.h>
 #include <mbedtls/sha256.h>
 #include <mbedtls/entropy.h>
@@ -20,7 +23,6 @@ static unsigned char master_key[32];
 static mbedtls_entropy_context entropy;
 static mbedtls_ctr_drbg_context ctr_drbg;
 
-// Функция получения уникального Hardware ID
 static void get_hardware_id(char* out_id) {
 #ifdef _WIN32
     DWORD serial;
@@ -39,18 +41,12 @@ static void get_hardware_id(char* out_id) {
 
 void kyro_crypto_init(void) {
     mkdir(STORAGE_PATH, 0777);
-    
     mbedtls_entropy_init(&entropy);
     mbedtls_ctr_drbg_init(&ctr_drbg);
-    
     const char* custom_seed = "Kyrogram_Secure_Seed_2026_NUA_PROD";
-    mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, 
-                         (const unsigned char*)custom_seed, strlen(custom_seed));
-
-    // Генерация мастер-ключа на основе Hardware ID
+    mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, (const unsigned char*)custom_seed, strlen(custom_seed));
     char hw_id[128] = {0};
     get_hardware_id(hw_id);
-    
     unsigned char raw_seed[256];
     sprintf((char*)raw_seed, "%s_SECRET_2026_%s", KYRO_AUTHOR, hw_id);
     mbedtls_sha256(raw_seed, strlen((char*)raw_seed), master_key, 0);
@@ -59,38 +55,28 @@ void kyro_crypto_init(void) {
 bool kyro_secure_save(const char* account_id, const unsigned char* data, size_t size) {
     mbedtls_aes_context aes;
     mbedtls_aes_init(&aes);
-    
     unsigned char iv[16];
     mbedtls_ctr_drbg_random(&ctr_drbg, iv, 16);
-    
     mbedtls_aes_setkey_enc(&aes, master_key, 256);
-    
     size_t padded_size = ((size + 15) / 16) * 16;
     unsigned char* buffer = (unsigned char*)malloc(padded_size);
     memset(buffer, 0, padded_size);
     memcpy(buffer, data, size);
-    
     unsigned char* encrypted_data = (unsigned char*)malloc(padded_size);
     unsigned char iv_copy[16];
     memcpy(iv_copy, iv, 16); 
-    
     mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, padded_size, iv_copy, buffer, encrypted_data);
-    
     char full_path[256];
     sprintf(full_path, "%s%s.dat", STORAGE_PATH, account_id);
-    
     FILE* f = fopen(full_path, "wb");
     if (!f) {
         free(buffer); free(encrypted_data); mbedtls_aes_free(&aes);
         return false;
     }
-    
     fwrite(iv, 1, 16, f);
     fwrite(encrypted_data, 1, padded_size, f);
     fclose(f);
-    
-    free(buffer);
-    free(encrypted_data);
+    free(buffer); free(encrypted_data);
     mbedtls_aes_free(&aes);
     return true;
 }
@@ -98,30 +84,23 @@ bool kyro_secure_save(const char* account_id, const unsigned char* data, size_t 
 unsigned char* kyro_secure_load(const char* account_id, size_t* out_size) {
     char full_path[256];
     sprintf(full_path, "%s%s.dat", STORAGE_PATH, account_id);
-    
     FILE* f = fopen(full_path, "rb");
     if (!f) return NULL;
-    
     fseek(f, 0, SEEK_END);
     size_t total_size = ftell(f);
     if (total_size < 16) { fclose(f); return NULL; }
     fseek(f, 0, SEEK_SET);
-    
     unsigned char iv[16];
     fread(iv, 1, 16, f);
-    
     size_t data_size = total_size - 16;
     unsigned char* encrypted_data = (unsigned char*)malloc(data_size);
     fread(encrypted_data, 1, data_size, f);
     fclose(f);
-    
     mbedtls_aes_context aes;
     mbedtls_aes_init(&aes);
     mbedtls_aes_setkey_dec(&aes, master_key, 256);
-    
     unsigned char* decrypted_data = (unsigned char*)malloc(data_size);
     mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, data_size, iv, encrypted_data, decrypted_data);
-    
     *out_size = data_size;
     free(encrypted_data);
     mbedtls_aes_free(&aes);
